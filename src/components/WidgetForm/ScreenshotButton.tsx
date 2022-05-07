55046c31057e789baf7afa5871a4e39d0f7994c4
Arthur Lozano <athlonlozano@gmail.com>
2022-05-07 16:50:37 -0300

    Commit para subir para vercel com remoção de import não utilizado

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,6 +1,6 @@
 import html2canvas from "html2canvas";
 import { Camera, Trash } from "phosphor-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Loading } from "../Loading";
 
 interface ScreenshotButtonProps {
